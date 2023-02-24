54c36faa35ce5924fd374b49579b86bb30482dac
Navya <mekala.navya@capgemini.com>
2023-02-24 15:31:12 +0530

    added headless config and json report

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/resources/features", glue= "steps", publish = true
-, plugin = {"pretty", "html:target/cucumber-reports.html"}, monochrome = true)
+, plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
+        monochrome = true)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
