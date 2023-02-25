49315e415cb93e4278e05810139d3545386600d6
Navya <mekala.navya@capgemini.com>
2023-02-26 00:22:09 +0530

    modified

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/resources/features", glue= "steps", publish = true
+@CucumberOptions(features = "src/test/java/features", glue= "steps", publish = true
 , plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
        monochrome = true)
 
